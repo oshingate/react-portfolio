f47bae48bcb0135779561beb8d5f1ef8c58085c1
oshingate <oshingate@gmail.com>
2021-09-17 15:02:41 +0530

    added projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,38 @@
+import data from '../constants/constant';
+
 const Projects = () => {
+  let { projects } = data;
   return (
     <>
       <h2>Projects</h2>
+
+      <div className='flex wrap '>
+        {projects.map((project) => {
+          return (
+            <article
+              className='card blog-card flex-48 flex-col justify-between'
+              key={project.title}
+            >
+              <div className='posted-on'>
+                <span>{project.postedOn}</span>
+              </div>
+              <div>
+                <div className='blog-card__img-div '>
+                  <img src={project.image} alt='Blog Img' />
+                </div>
+
+                <span className='blog-card__title'>{project.title}</span>
+
+                <p className='blog-card__desc'>{project.desc}</p>
+              </div>
+
+              <a href={project.url} target='_blank' rel='noreferrer'>
+                Go To Project
+              </a>
+            </article>
+          );
+        })}
+      </div>
     </>
   );
 };
